91e957dda8ee8116ab5f80af89c57422f89910ac
Shikhar <Shikharsama@gmail.com>
2023-06-05 15:58:44 +0530

    second commit

---
+++ Scrpit.js
@@ -0,0 +1,21 @@
+console.log("Sup dude")
+
+// ways to declare a valriable 
+// var let const 
+
+let firstName = " Shikhar"
+
+let lastName = "Sama"
+
+console.log(firstName,lastName)
+console.log()
+
+// let string = "developer"
+
+// let new_String = string.charAt(0).toUpperCase()+string.substring(1)
+// console.log(new_String)
+
+
+// 
+
+
